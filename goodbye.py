f68aaef160ca5e35ecc235204c1006544f5034d2
Luke <lukediliberto@users.noreply.github.com>
2022-09-14 09:18:42 -0500

    Update goodbye.py

--- goodbye.py
+++ goodbye.py
@@ -1,1 +1,13 @@
-print("Goodbye from python.")
+import argparse
+import json
+
+def main(R1_file):
+    res = json.loads(R1_file)
+    print(res)
+
+if __name__ == '__main__':
+    parser=argparse.ArgumentParser(description="a script to do stuff")
+    parser.add_argument("R1_file")
+    args=parser.parse_args()
+    
+    main(args.R1_file)
